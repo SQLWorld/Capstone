57c69c539886a7590f48b3ecd1ada06520c40e01
SQLWorld <92478253+SQLWorld@users.noreply.github.com>
2026-06-03 22:16:50 -0400

    Change router from HashRouter to BrowserRouter

--- argyle-website/src/App.jsx
+++ argyle-website/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import ErrorBoundary from './components/ErrorBoundary';
 import { ThemeProvider } from './components/ThemeProvider';
